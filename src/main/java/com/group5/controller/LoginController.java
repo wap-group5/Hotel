ac2f90410f10574e0b64b700dbd96ef78cc05397
Sovichea Cheth <scheth@mum.edu>
2019-07-13 12:20:31 -0500

    Update LoginController
    Update LoginController

--- src/main/java/com/group5/controller/LoginController.java
+++ src/main/java/com/group5/controller/LoginController.java
@@ -1,13 +1,32 @@
 package com.group5.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class LoginController extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+public class LoginController extends HttpServlet {
 
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+
+        if(user.equals("admin") && pass.equals("password")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", user);
+
+            //If the user login successful, send to the check in page
+            response.sendRedirect("checkin.jsp");
+
+        } else {
+            response.sendRedirect("login.jsp");
+        }
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
 }
