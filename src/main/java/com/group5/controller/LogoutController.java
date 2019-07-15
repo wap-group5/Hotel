d49ac1c6456224d9d1554fde1f6f3f045b098ac5
Sovichea Cheth <scheth@mum.edu>
2019-07-15 12:35:55 -0500

    Update Logout Controller
    Update Logout Controller

--- src/main/java/com/group5/controller/LogoutController.java
+++ src/main/java/com/group5/controller/LogoutController.java
@@ -1,12 +1,14 @@
 package com.group5.controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebServlet("/logout")
 public class LogoutController extends HttpServlet {
 
     @Override
